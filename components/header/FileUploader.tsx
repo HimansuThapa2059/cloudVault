938f14c515b7680164767f0c54ef99e45612f77a
Himansu <himansuthapa2059@gmail.com>
2024-11-13 17:26:51 +0545

    bug: make sure to redirect when there is no session

--- components/header/FileUploader.tsx
+++ components/header/FileUploader.tsx
@@ -1,7 +1,94 @@
-import React from "react";
+"use client";
+import React, { useCallback, useState } from "react";
+import { useDropzone } from "react-dropzone";
+import { Button } from "../ui/button";
+import { cn, convertFileToUrl, getFileType } from "@/lib/utils";
+import Image from "next/image";
+import Thumbnail from "../Thumbnail";
 
-const FileUploader = () => {
-  return <div>FileUploader</div>;
+interface FileUploaderProps {
+  ownerId: string;
+  accountId: string;
+  className?: string;
+}
+
+const FileUploader = ({
+  ownerId,
+  accountId,
+  className = "",
+}: FileUploaderProps) => {
+  const [files, setFiles] = useState<File[]>([]);
+  const onDrop = useCallback(async (acceptedFiles: File[]) => {
+    setFiles(acceptedFiles);
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  const handleRemoveFile = (
+    e: React.MouseEvent<HTMLImageElement>,
+    fileName: string,
+  ) => {
+    e.stopPropagation();
+    setFiles((cur) => cur.filter((file) => file.name !== fileName));
+  };
+
+  return (
+    <div {...getRootProps()} className="cursor-pointer">
+      <input {...getInputProps()} />
+      <Button type="button" className={cn("uploader-button", className)}>
+        <Image src="icons/upload.svg" alt="upload" height={24} width={24} />
+        <p>Upload</p>
+      </Button>
+
+      {files.length > 0 && (
+        <ul className="uploader-preview-list">
+          {files.map((file, index) => {
+            const { type, extension } = getFileType(file.name);
+
+            return (
+              <li
+                key={`${file.name}-${index}`}
+                className="uploader-preview-item"
+              >
+                <div className="flex items-center gap-3">
+                  <Thumbnail
+                    type={type}
+                    extension={extension}
+                    url={convertFileToUrl(file)}
+                  />
+
+                  <div className="preview-item-name space-y-1">
+                    <span className="block w-[calc(100%)] truncate">
+                      {file.name}
+                    </span>
+                    <Image
+                      src="/icons/file-loader.gif"
+                      width={80}
+                      height={26}
+                      alt="Loader"
+                    />
+                  </div>
+                </div>
+
+                <Image
+                  src="/icons/remove.svg"
+                  width={24}
+                  height={24}
+                  alt="Remove"
+                  onClick={(e) => handleRemoveFile(e, file.name)}
+                />
+              </li>
+            );
+          })}
+        </ul>
+      )}
+
+      {isDragActive ? (
+        <p>Drop the files here ...</p>
+      ) : (
+        <p>Drag 'n' drop some files here, or click to select files</p>
+      )}
+    </div>
+  );
 };
 
 export default FileUploader;
