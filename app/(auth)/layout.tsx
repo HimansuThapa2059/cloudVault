c9bfd793dadc7666fd0c874215d8be3abe39db18
Himansu <himansuthapa2059@gmail.com>
2024-11-16 21:54:32 +0545

    minor fixes

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -6,13 +6,17 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
     <div className="flex min-h-screen">
       <section className="hidden w-1/2 items-center justify-center bg-brand p-10 lg:flex xl:w-2/5">
         <div className="flex max-h-[800px] max-w-[430px] flex-col justify-center space-y-12">
-          <Image
-            src="/icons/logo-full.svg"
-            alt="logo"
-            width={224}
-            height={82}
-            className="h-auto"
-          />
+          <div className="hidden items-center lg:flex">
+            <Image
+              src="/icons/logo-brand-2.svg"
+              alt="logo"
+              height={80}
+              width={80}
+              className="h-auto"
+            />
+
+            <span className="h1 ml-2 font-semibold text-white">cloudVault</span>
+          </div>
 
           <div className="space-y-5 text-white">
             <h1 className="h1">Manage your files the best way</h1>
@@ -32,14 +36,19 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
 
       <section className="flex flex-1 flex-col items-center bg-white p-4 py-10 lg:justify-center lg:p-10 lg:py-0">
         <div className="mb-16 lg:hidden">
-          <Image
-            src="icons/logo-full-brand.svg"
-            alt="logo"
-            width={224}
-            height={82}
-            className="h-auto w-[200px] lg:w-[250px]"
-            priority
-          />
+          <div className="flex items-center">
+            <Image
+              src="/icons/logo-brand.svg"
+              alt="logo"
+              height={74}
+              width={74}
+              className="h-auto"
+            />
+
+            <span className="ml-2 text-3xl font-semibold text-brand">
+              cloudVault
+            </span>
+          </div>
         </div>
 
         {children}
